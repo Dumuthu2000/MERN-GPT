8a9ceab46a8cabd0b10ec0da12051b52cdaa4b75
Dumuthu Jayasinghe <dumuthujayasinghe@gmail.com>
2023-11-10 15:28:58 +0530

    added morgan dependency

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import morgan from 'morgan';
 
 const app = express();
 
 //middlewares
 app.use(express.json());
 
+//should remove in production
+app.use(morgan("dev"));
 export default app;
